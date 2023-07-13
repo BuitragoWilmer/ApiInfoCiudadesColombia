bdbf031dd23fb84937f4d98ae7e09d2c1d08ad97
WilmerBuitrago <wilmerab.20@gmail.com>
2023-07-12 21:40:51 -0500

    Se crea controlador de archivos

---
+++ Controllers/FilesController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using System.IO;
+using System.Net;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace InfoCity.API.Controllers
+{
+
+    [Route("api/archivos")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _fileExtension;
+
+        public FilesController(FileExtensionContentTypeProvider file)
+        {
+            _fileExtension = file
+            ?? throw new ArgumentNullException(nameof(file));
+        }
+
+            [HttpGet("{assetid}")]
+        public ActionResult GetFile(string assetid)
+        {
+
+            Cloudinary cloudinary = new Cloudinary("cloudinary://959377429313512:9lb_08in6JRZ9Bo7KT5BZFOlXd4@dvejlclzb");
+            var result = cloudinary.GetResourceByAssetId(assetid);
+            Stream remoteStream = null;
+            WebResponse response = null;
+            if (result.StatusCode == System.Net.HttpStatusCode.BadRequest){
+                return NotFound();
+            }
+            WebRequest request = WebRequest.Create(result.SecureUrl);
+            byte[] byteArray= new byte[1024];
+            var pathToFile = result.PublicId +"."+ result.Format;
+            if (request != null)
+            {
+                var sizewebrequest = HttpWebRequest.Create(result.SecureUrl);
+                sizewebrequest.Method = "HEAD";
+                response = request.GetResponse();
+                
+                if(response != null)
+                {
+                    remoteStream = response.GetResponseStream();
+                    MemoryStream ms = new MemoryStream();
+                    remoteStream.CopyTo(ms);
+                    byteArray = ms.ToArray();
+                }
+            }
+            if(!_fileExtension.TryGetContentType(pathToFile, out string contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+            return File(byteArray, contentType, Path.GetFileName(pathToFile));
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,11 +28,15 @@ namespace InfoCity.API
         public void ConfigureServices(IServiceCollection services)
         {
 
-            services.AddControllers();
+            services.AddControllers(options =>
+            {
+                options.ReturnHttpNotAcceptable = true;
+            }).AddXmlDataContractSerializerFormatters();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "InfoCity.API", Version = "v1" });
             });
+            services.AddSingleton<FileExtensionContentTypeProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
